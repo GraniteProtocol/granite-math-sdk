fe765e5942bd83325fed30a05ad0df1fe764bae7
OxMarco <>
2024-07-24 12:22:40 +0200

    test apy

---
+++ test/apy.test.ts
@@ -0,0 +1,28 @@
+import { InterestRateParams } from "../src";
+import { calculateBorrowAPY, calculateLpAPY } from "../src/functions";
+
+test('calculateLpAPY', () => {
+  function testApyParams(): InterestRateParams {
+    return {
+      urKink: 0.8,
+      baseIR: 0.15,
+      slope1: 0,
+      slope2: 0,
+    };
+  }
+
+  expect(calculateLpAPY(0, testApyParams(), 0)).toBe(0);
+});
+
+test('calculateBorrowAPY', () => {
+  function testApyParams(): InterestRateParams {
+    return {
+      urKink: 0.8,
+      baseIR: 0.15,
+      slope1: 0,
+      slope2: 0,
+    };
+  }
+
+  expect(calculateBorrowAPY(0, testApyParams())).toBeCloseTo(0.16); // equals to baseIR + compounded interest
+});
