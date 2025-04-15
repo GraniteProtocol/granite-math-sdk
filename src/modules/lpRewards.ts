911fe93b2611c01f37685dc921616acdce0edb07
OxMarco <giammarco.casanova@gmail.com>
2025-04-15 17:39:14 +0200

    fix lp apy calc

--- src/modules/lpRewards.ts
+++ src/modules/lpRewards.ts
@@ -6,66 +6,27 @@
  */
 
 import { secondsInAYear } from "../constants";
-import { Epoch, Snapshot } from "../types";
+import { Epoch } from "../types";
 
 /**
- * Calculates the total rewards for the current epoch for a given LP
- * @param epoch - Current epoch data
- * @param snapshots - Array of snapshots for the current epoch
- * @returns The total rewards for the LP
- */
-export function earnedRewards(epoch: Epoch, snapshots: Snapshot[]): number {
-  if (snapshots.length < 2)
-    throw new Error("Insufficient data to compute rewards");
-
-  const epochDuration = epoch.endTimestamp - epoch.startTimestamp;
-  if (epochDuration == 0) throw new Error("Invalid epoch duration");
-
-  let totalEarnedRewards = 0;
-  for (let i = 1; i < snapshots.length; i++) {
-    const prevSnapshot = snapshots[i - 1];
-    const snapshot = snapshots[i];
-
-    const percentOfEpoch =
-      (snapshot.timestamp - prevSnapshot.timestamp) / epochDuration;
-
-    const percentOfLpShares = snapshot.userLpShares / snapshot.totalLpShares;
-
-    totalEarnedRewards +=
-      percentOfEpoch * percentOfLpShares * epoch.totalRewards;
-  }
-
-  return totalEarnedRewards;
-}
-
-/**
- * Calculates the total rewards for the current epoch for all LPs
- * @param epoch - Current epoch data
- * @returns The total rewards for all of the LPs
- */
-export function totalLpRewards(epoch: Epoch): number {
-  const epochDuration = epoch.endTimestamp - epoch.startTimestamp;
-  if (epochDuration == 0) throw new Error("Invalid epoch duration");
-
-  return epoch.targetAPR * (epochDuration / secondsInAYear) * epoch.cap;
-}
-
-/**
- * Calculates the actual APR based on rewards and deposit amount
- * @param rewards - Total rewards earned
- * @param depositAmount - Amount of tokens deposited
- * @param timeInSeconds - Time period in seconds
+ * Calculates the actual APR based on epoch and deposit amount
+ * @param totalLpShares - Total LP shares in the market
+ * @param userLpShares - Amount of user deposited assets in shares terms
+ * @param epoch - The current epoch
  * @returns The actual APR scaled as a decimal (e.g., 0.05 for 5% APR)
  */
 export function calculateApr(
-  rewards: number,
-  depositAmount: number,
-  timeInSeconds: number,
+  totalLpShares: number,
+  userLpShares: number,
+  epoch: Epoch,
 ): number {
-  if (depositAmount <= 0) throw new Error("Deposit amount must be positive");
-  if (timeInSeconds <= 0) throw new Error("Time period must be positive");
-
-  return (rewards / depositAmount) * (secondsInAYear / timeInSeconds);
+  if (userLpShares <= 0)
+    throw new Error("User LP shares amount must be positive");
+  const shareRatio = userLpShares / totalLpShares;
+  const perSecondRewards =
+    epoch.totalRewards / (epoch.endTimestamp - epoch.startTimestamp);
+  const userPerSecondRewards = shareRatio * perSecondRewards;
+  return (userPerSecondRewards * secondsInAYear) / userLpShares;
 }
 
 /**
